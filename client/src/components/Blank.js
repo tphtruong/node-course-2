e3bb11cc5ee633b1e8839d48437fec303249aeec
hoa truong <tphtruong@yahoo.com>
2018-09-29 22:49:05 +1000

    test

--- client/src/components/Blank.js
+++ client/src/components/Blank.js
@@ -9,7 +9,7 @@ const Blank = (props) => {
                 <p className="App-title">You win some, you lose some.</p>
                 <p>But with Xap Xam Chuong</p>
                 <p>Everyone wins !!!</p>
-                <p>Except for MR TRUONG</p>
+                <p>Power by Hoa TRUONG (using Node with React Fullstack)</p>
             </div>
         </div>
     )
